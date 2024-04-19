bf455c858c9debe8c23e71eac279f709bbd6a623
riyavish13 <167068688+riyavish13@users.noreply.github.com>
2024-04-19 12:26:53 +0530

    IV2-25 <Input Field>

--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -1,5 +1,4 @@
 import React, { InputHTMLAttributes } from "react";
-import MailLineIcon from "remixicon-react/MailLineIcon";
 import { cn } from "../utils/utils";
 
 interface InputProps
@@ -20,19 +19,31 @@ const Input = ({
   ...props
 }: InputProps) => {
   return (
-    <div className={cn("relative flex items-center", size ==="sm"?"w-[320px] h-[40px]":"w-[313px] h-[44px]")}>
-      <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+    <div
+      className={cn(
+        "group flex items-center gap-2 border border-gray-200 rounded-lg bg-gray-50 shadow-xs hover:bg-gray-50 hover:border-gray-300 focus-within:border-gray-800  focus-within:bg-gray-25  focus-within:hover:bg-gray-50 focus-within:hover:border-gray-800 has-[:disabled]:bg-gray-400 has-[:disabled]:border-gray-400 has-[:disabled]:pointer-events-none", size === "sm"? "w-[320px] py-2.5 px-3.5 h-10":"p-2.5 w-[313px] h-11",className
+      )}
+    >
+      <span
+        className={cn(
+          "group-hover:text-gray-600 group-focus-within:text-gray-600",
+          disabled === true && "text-gray-900"
+        )}
+      >
         {startIcon}
       </span>
       <input
         {...props}
-        type={type}
         disabled={disabled}
-        className={cn(
-          "w-full px-3.5 py-2.5 pr-10 rounded-lg border border-gray-200 shadow-sm text-sm text-gray-500 bg-transparent hover:bg-gray-50 hover:border-gray-300 active:bg-gray-25 active:border-gray-800 active:text-gray-900 pl-10 hover:focus:border-gray-800 hover:focus:bg-gray-50 focus:outline-none focus:text-gray-900 focus:border-gray-800 group disabled:pointer-events-none disabled:opacity-30 disabled:bg-gray-400 placeholder:disabled:text-gray-900 disabled:border-gray-400 disabled:text-gray-900 invalid:border-error-400",className,size
-        )}
+        type={type}
+        className={cn("w-full text-sm focus:outline-none bg-transparent disabled:text-gray-900 placeholder:text-gray-500 group-hover:placeholder:text-gray-500", size)}
       />
-      <span className="absolute inset-y-0 right-0 flex items-center pr-3 disabled:border-gray-400 disabled:text-gray-100 invalid:text-error-400">{endIcon}</span>
+      <span className={cn(
+          "group-hover:text-gray-600 group-focus-within:text-gray-600",
+          disabled === true && "text-gray-900"
+        )}>
+        {endIcon}
+      </span>
     </div>
   );
 };
