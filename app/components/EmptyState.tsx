3adfb8c94bf89d7d657c6d5884d02f6e26f9cf80
Kajal Sharma <kajal.sharma@atomostech.com>
2024-04-30 14:47:48 +0530

    updated empty state

--- app/components/EmptyState.tsx
+++ app/components/EmptyState.tsx
@@ -26,9 +26,9 @@ export const EmptyImageSVG = () => {
         d="M64.9248 56.6069C64.9248 56.6069 65.0112 56.5529 65.0436 56.5204L71.539 52.1974L68.4048 50.5006L65.2706 51.2572L56.9704 19.5802C56.203 16.6406 53.2742 14.8141 50.2913 15.4301L10.7033 23.5034L9.30911 20.38L5.89395 26.2593C5.8183 26.3782 5.74263 26.5079 5.67779 26.6267V26.6484C5.00772 27.902 4.79152 29.4151 5.22382 30.8957L15.664 67.5441C16.053 68.9167 17.4688 69.7164 18.8414 69.3382L64.1142 56.996C64.3844 56.9203 64.633 56.7906 64.8599 56.6501L64.914 56.6177L64.9248 56.6069Z"
         fill="#F2F4F7"
         stroke="#1570EF"
-        stroke-width="0.7"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="0.7"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M70.7622 52.5535L23.0902 65.555C21.6419 65.9441 20.1505 65.1119 19.7398 63.6637L8.74853 25.07C7.78666 21.698 9.90494 18.218 13.3417 17.5155L56.2153 8.77221C59.3495 8.13457 62.4404 10.0583 63.2402 13.1493L72.686 49.2248C73.0642 50.673 72.2104 52.1645 70.7622 52.5535Z"
@@ -53,60 +53,60 @@ export const EmptyImageSVG = () => {
       <path
         d="M70.7622 52.5535L23.0902 65.555C21.6419 65.9441 20.1505 65.1119 19.7398 63.6637L8.74853 25.07C7.78666 21.698 9.90494 18.218 13.3417 17.5155L56.2153 8.77221C59.3495 8.13457 62.4404 10.0583 63.2402 13.1493L72.686 49.2248C73.0642 50.673 72.2104 52.1645 70.7622 52.5535Z"
         stroke="#1570EF"
-        stroke-width="0.7"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="0.7"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M33.0003 40.8058L25.943 42.5134C25.5107 42.6215 25.2513 43.0646 25.3702 43.4969L28.7097 55.4176L37.2153 53.202L33.9406 41.357C33.8325 40.9463 33.411 40.6977 32.9895 40.795L33.0003 40.8058Z"
         stroke="#1570EF"
-        stroke-width="0.7"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="0.7"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M43.2152 27.7587L36.4064 29.3042C35.9633 29.4015 35.7039 29.8446 35.8228 30.2877L41.7886 52.0109L50.0023 49.871L44.1662 28.3207C44.0582 27.91 43.6475 27.6615 43.226 27.7587H43.2152Z"
         fill="#1570EF"
         stroke="#1570EF"
-        stroke-width="0.7"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="0.7"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M57.285 30.6058L50.7896 32.1188C50.3573 32.2161 50.0871 32.6592 50.206 33.0915L54.3993 48.73L62.3321 46.6658L58.236 31.1678C58.128 30.7571 57.7065 30.5085 57.2958 30.6058H57.285Z"
         fill="#F2F4F7"
         stroke="#1570EF"
-        stroke-width="0.7"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="0.7"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M22.3553 60.2827L69.1952 47.8972"
         stroke="#1570EF"
-        stroke-width="0.7"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="0.7"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M26.5596 47.6014L30.8657 41.3244"
         stroke="#1570EF"
-        stroke-width="0.7"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="0.7"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M34.4999 43.7638L28.0771 53.1231"
         stroke="#1570EF"
-        stroke-width="0.7"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="0.7"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M32.4999 54.4171L36.0926 49.1731"
         stroke="#1570EF"
-        stroke-width="0.7"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="0.7"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
