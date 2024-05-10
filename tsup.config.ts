712f8c1d028735ab7db2968ae12bbdd7409fe726
Kajal Sharma <kajal.sharma@atomostech.com>
2024-05-10 09:33:53 +0530

    updated tsup

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,17 +1,10 @@
 import { defineConfig } from "tsup";
-import esbuildCssModulesPlugin from 'esbuild-css-modules-plugin';
 
 export default defineConfig({
   format: ["cjs", "esm"],
   entry: ["./app/index.ts"],
   dts: true,
-  splitting: false,
-  sourcemap: true,
   shims: true,
   skipNodeModulesBundle: true,
   clean: true,
-  outDir: 'dist',
-  plugins: [
-    esbuildCssModulesPlugin()
-  ]
 });
