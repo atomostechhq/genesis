20fe92d9ce64af6771a15c2302cb4016c5454010
Kajal Sharma <kajal.sharma@atomostech.com>
2024-06-21 10:57:00 +0530

    updated library with changes

--- app/components/Tooltip.tsx
+++ app/components/Tooltip.tsx
@@ -50,16 +50,16 @@ interface TooltipProps
 }
 
 const tooltipVariants = cva(
-  "bg-white shadow-lg rounded-lg absolute hidden group-hover:inline-block p-3 z-10 max-w-[328px] w-max whitespace-wrap transition-opacity transition-transform duration-300 opacity-0 group-hover:opacity-100",
+  "bg-white shadow-lg rounded-lg absolute hidden group-hover:block p-3 z-10 max-w-[328px] w-max whitespace-wrap opacity-0 group-hover:opacity-100 transform transition-all duration-500 ease-in-out delay-200",
   {
     variants: {
       position: {
-        top: "bottom-[calc(100%+0px)] translate-y-[-10px] group-hover:translate-y-0",
+        top: "bottom-[calc(100%+0px)] group-hover:translate-y-0 translate-y-[-10px]",
         right:
-          "top-1/2 -translate-y-1/2 left-[calc(100%+0px)] translate-x-[-10px] group-hover:translate-x-0",
+          "top-1/2 -translate-y-1/2 left-[calc(100%+0px)] group-hover:translate-x-0 translate-x-[-10px]",
         bottom:
-          "top-[calc(100%+0px)] translate-y-[10px] group-hover:translate-y-0",
-        left: "top-1/2 -translate-y-1/2 right-[calc(100%+0px)] translate-x-[10px] group-hover:translate-x-0",
+          "top-[calc(100%+0px)] group-hover:translate-y-0 translate-y-[10px]",
+        left: "top-1/2 -translate-y-1/2 right-[calc(100%+0px)] group-hover:translate-x-0 translate-x-[10px]",
       },
     },
   }
@@ -67,7 +67,7 @@ const tooltipVariants = cva(
 
 const Tooltip = ({ position, content, children }: TooltipProps) => {
   return (
-    <div id="tooltip" className="relative cursor-pointer px-2 group">
+    <div id="tooltip" className="relative cursor-pointer px-2 group transition-all delay-200 ease-out">
       <div>{children}</div>
       <span className={cn(tooltipVariants({ position }))}>{content}</span>
     </div>
