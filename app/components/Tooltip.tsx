efa8bc958f9405798bb802ae82178e914e7bcbb6
Riya Vishwakarma <riya.vishwakarma@atomostech.com>
2024-07-18 09:36:23 +0530

    tooltip animation

--- app/components/Tooltip.tsx
+++ app/components/Tooltip.tsx
@@ -3,19 +3,12 @@ import { HTMLAttributes, ReactNode } from "react";
 import { cn } from "../utils/utils";
 import React from "react";
 
-interface TooltipProps
-  extends Omit<HTMLAttributes<HTMLDivElement>, "content">,
-    VariantProps<typeof tooltipVariants> {
-  children: ReactNode;
-  position: "top" | "right" | "bottom" | "left";
-  content: JSX.Element | string;
-}
 const tooltipVariants = cva(
-  "bg-white shadow-lg rounded-lg absolute hidden group-hover:block p-3 z-10 max-w-[328px] w-max whitespace-wrap opacity-0 group-hover:opacity-100 transform transition-all duration-500 ease-in-out delay-200",
+  "bg-white shadow-lg rounded-lg absolute hidden group-hover:block p-3 z-10 max-w-[328px] w-max whitespace-normal opacity-0 group-hover:opacity-100 transform transition-all duration-1000 ease-in-out group-hover:delay-[2000ms]",
   {
     variants: {
       position: {
-        top: "bottom-[calc(100%+0px)] group-hover:translate-y-0 translate-y-[-10px]",
+        top: "bottom-[calc(100%+0px)] group-hover:translate-y-0 delay-1000 translate-y-[-10px]",
         right:
           "top-1/2 -translate-y-1/2 left-[calc(100%+0px)] group-hover:translate-x-0 translate-x-[-10px]",
         bottom:
@@ -26,12 +19,22 @@ const tooltipVariants = cva(
   }
 );
 
-const Tooltip = ({ position, content, children }: TooltipProps) => {
+interface TooltipProps
+  extends Omit<HTMLAttributes<HTMLDivElement>, "content">,
+    VariantProps<typeof tooltipVariants> {
+  children: ReactNode;
+  position: "top" | "right" | "bottom" | "left";
+  content: JSX.Element | string;
+}
+
+const Tooltip: React.FC<TooltipProps> = ({
+  position,
+  content,
+  children,
+  ...props
+}) => {
   return (
-    <div
-      id="tooltip"
-      className="relative cursor-pointer px-2 group transition-all delay-200 ease-out"
-    >
+    <div {...props} className="relative cursor-pointer text-sm group">
       <div>{children}</div>
       <span className={cn(tooltipVariants({ position }))}>{content}</span>
     </div>
