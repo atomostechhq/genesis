2280a598684c723a6413b63089531b6719d80b04
KajalS6398 <kajal.sharma@atomostech.com>
2025-01-10 15:20:24 +0530

    added use client, ready for publish

--- app/components/Chip.tsx
+++ app/components/Chip.tsx
@@ -1,82 +1,3 @@
-// import React, { ReactNode, HTMLAttributes } from "react";
-// import { cn } from "../utils/utils";
-// import { cva, VariantProps } from "class-variance-authority";
-
-// interface ChipProps
-//   extends HTMLAttributes<HTMLDivElement>,
-//     VariantProps<typeof chipVariants> {
-//   children: ReactNode;
-//   dot?: boolean;
-//   dotColor?: string;
-// }
-
-// const chipVariants = cva(
-//   "rounded-full capitalize flex items-center w-fit gap-2",
-//   {
-//     variants: {
-//       intent: {
-//         default: "bg-gray-50 text-gray-600",
-//         success: "bg-success-50 text-success-600",
-//         warning: "bg-warning-50 text-warning-500",
-//         error: "bg-error-50 text-error-600",
-//         primary: "bg-primary-100 text-primary-700",
-//         bluegray: "bg-bluegray-100 text-bluegray-500",
-//         bluelight: "bg-bluelight-100 text-bluelight-600",
-//         violet: "bg-violet-50 text-violet-700",
-//         indigo: "bg-indigo-100 text-indigo-700",
-//         purple: "bg-purple-50 text-purple-700",
-//         pink: "bg-pink-25 text-pink-700",
-//         rose: "bg-rose-50 text-rose-600",
-//         orange: "bg-orange-50 text-orange-600",
-//       },
-//       size: {
-//         sm: "text-xs px-2 py-0.5",
-//         md: "text-sm px-2.5 py-0.5",
-//         lg: "text-sm px-3 py-1",
-//       },
-//     },
-//     defaultVariants: {
-//       intent: "default",
-//       size: "md",
-//     },
-//   }
-// );
-
-// const Chip = ({
-//   children,
-//   className,
-//   size,
-//   intent,
-//   dot,
-//   dotColor,
-// }: ChipProps) => {
-//   return (
-//     <div className={cn(chipVariants({ intent, className, size }))}>
-//       {dot && (
-//         <span
-//           className={cn(
-//             "w-1.5 rounded-full h-1.5",
-//             intent === "default"
-//               ? "bg-gray-600"
-//               : intent === "success"
-//               ? "bg-success-600"
-//               : intent === "warning"
-//               ? "bg-warning-600"
-//               : intent === "error"
-//               ? "bg-error-600"
-//               : "bg-primary-600",
-//             dotColor
-//           )}
-//         ></span>
-//       )}
-//       <span>{children}</span>
-//     </div>
-//   );
-// };
-
-// export default Chip;
-
-
 import React, { ReactNode, HTMLAttributes } from "react";
 import { cn } from "../utils/utils";
 import { cva, VariantProps } from "class-variance-authority";
@@ -114,7 +35,6 @@ const chipVariants = cva(
   }
 );
 
-// Mapping for dot color variants
 const dotColorVariants: Record<string, string> = {
   default: "bg-gray-600",
   success: "bg-success-600",
--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,48 +1,4 @@
-// import React, { ReactNode } from "react";
-// import { RiCloseLine } from "@remixicon/react";
-
-// interface ModalProps {
-//   children?: ReactNode;
-//   showModal?: boolean;
-//   closeModal?: boolean;
-//   setShowModal?: any;
-//   closeOnOutsideClick?: boolean;
-// }
-
-// export default function Modal({
-//   children,
-//   showModal,
-//   setShowModal,
-//   closeModal = true,
-//   closeOnOutsideClick = true,
-// }: ModalProps) {
-//   const handleClickOutside = (event: React.MouseEvent<HTMLDivElement>) => {
-//     if (event.target === event.currentTarget && closeOnOutsideClick) {
-//       setShowModal(false);
-//     }
-//   };
-//   return (
-//     <>
-//       {showModal && (
-//         <div onClick={handleClickOutside} className="w-full h-full bg-backdrop bg-blend-overlay fixed top-0 bottom-0 left-0 right-0 flex justify-center items-center z-[1000000] overflow-hidden">
-//           <div className="relative bg-primary-400 shadow-boxShadow rounded-xl p-[18px] transition-all duration-150 fade-in-grow">
-//             <div>{children}</div>
-//             {closeModal && (
-//               <div
-//                 className="absolute top-4 ml-5 right-5 z-10 shadow-backdrop rounded-full text-primary cursor-pointer hover:bg-primaryLight"
-//                 onClick={() => setShowModal((prev: boolean) => !prev)}
-//               >
-//                 <RiCloseLine size={24} />
-//               </div>
-//             )}
-//           </div>
-//         </div>
-//       )}
-//     </>
-//   );
-// }
-
-
+"use client";
 import React, { ReactNode, useEffect } from "react";
 import { RiCloseLine } from "@remixicon/react";
 
@@ -61,18 +17,17 @@ export default function Modal({
   setShowModal,
   closeModal = true,
   closeOnOutsideClick = true,
-  className = "", // Default to empty string if no custom class is provided
+  className = "", 
 }: ModalProps) {
-  // Disable body scrolling when modal is open
   useEffect(() => {
     if (showModal) {
-      document.body.style.overflow = "hidden"; // Disable scrolling
+      document.body.style.overflow = "hidden"; 
     } else {
-      document.body.style.overflow = "auto"; // Re-enable scrolling when modal is closed
+      document.body.style.overflow = "auto"; 
     }
 
     return () => {
-      document.body.style.overflow = "auto"; // Cleanup on unmount
+      document.body.style.overflow = "auto"; 
     };
   }, [showModal]);
 
